a05febe0699ba18dfa7fb445b9d0ef441fd83885
Martin Lindhe <martin-commit@ubique.se>
2017-03-04 09:13:38 +0100

    install.ps1: fix wix download url

--- installer/build.ps1
+++ installer/build.ps1
@@ -45,7 +45,7 @@ mkdir -Force Work,Output | Out-Null
 Write-Verbose "Downloading files"
 # Somewhat obscure url, points to WiX 3.10 binary release
 Write-Verbose "Downloading WiX..."
-Get-FileIfNotExists "http://download-codeplex.sec.s-msft.com/Download/Release?ProjectName=wix&DownloadId=1504735&FileTime=130906491728530000&Build=21031" "$sourceDir\wix-binaries.zip"
+Get-FileIfNotExists "http://download-codeplex.sec.s-msft.com/Download/Release?ProjectName=wix&DownloadId=1504735&FileTime=130906491728530000&Build=21040" "$sourceDir\wix-binaries.zip"
 mkdir -Force WiX | Out-Null
 Expand-Archive -Path "${sourceDir}\wix-binaries.zip" -DestinationPath WiX -Force
 
