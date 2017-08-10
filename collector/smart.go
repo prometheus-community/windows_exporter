b1d83ac04fbb1945123bba3ad6b90399e855dc52
Martin Lindhe <martin-commit@ubique.se>
2017-08-10 08:11:25 +0200

    wip: handle smart data

--- collector/smart.go
+++ collector/smart.go
@@ -1,9 +1,11 @@
 // returns data points from MSStorageDriver_ATAPISmartData class
+// parts are based on https://exchange.nagios.org/directory/Plugins/Operating-Systems/Windows/NRPE/check_smartwmi-SMART-Monitoring-for-Windows-by-using-builtin-WMI/details by Thomas Rechberger
 
 package collector
 
 import (
 	"log"
+	"strings"
 
 	"github.com/StackExchange/wmi"
 	"github.com/prometheus/client_golang/prometheus"
@@ -12,6 +14,11 @@ import (
 func init() {
 	Factories["smart"] = NewSMARTCollector
 }
+
+// ...
+const (
+	DiskOldAge = 30000 // age in hours when the disk is considered to be old and prone to errors, a warning will then be generated. more suited for hdd
+)
 
 // A SMARTCollector is a Prometheus collector for WMI metrics
 type SMARTCollector struct {
@@ -62,7 +69,11 @@ type MSStorageDriver_ATAPISmartData struct {
 	SelfTestStatus  uint64
 	TotalTime       uint64
 	SmartCapability uint64
-	VendorSpecific  []uint8 // TODO parse this when https://github.com/StackExchange/wmi/pull/30 is merged
+	VendorSpecific  []uint8 // TODO depends on https://github.com/StackExchange/wmi/pull/30
+}
+
+type MSStorageDriver_FailurePredictStatus struct {
+	PredictFailure bool // XXX read, see getfailurepredict()
 }
 
 func (c *SMARTCollector) collect(ch chan<- prometheus.Metric) (*prometheus.Desc, error) {
@@ -71,7 +82,10 @@ func (c *SMARTCollector) collect(ch chan<- prometheus.Metric) (*prometheus.Desc,
 		return nil, err
 	}
 
-	poharg := "hour" // XXX add as command line arg, either "hour" (default), "min" or "sec"
+	poharg := "hour"    // XXX add as command line arg, either "hour" (default), "min" or "sec"
+	hddhealthcalc := "" // XXX as cli arg, can be "restricted" too
+	tempcritarg := 0    // XXX as cli arg
+	tempwarnarg := 0    // XXX as cli arg
 
 	for _, disk := range dst {
 		if !disk.Active {
@@ -103,9 +117,16 @@ func (c *SMARTCollector) collect(ch chan<- prometheus.Metric) (*prometheus.Desc,
 		rawreaderror := 0
 		attrcriterror := 0
 		hddattrcriterror := 0
-		seekerror := 0
 		dmacrcerror := 0
 		softreaderror := 0
+		healtherror := 0
+		sumattrcriterror := 0
+		agewarnerror := 0
+		hddcriterror := 0
+		predicterror := 0
+		tempcriterror := 0
+		tempwarnerror := 0
+
 		programfailcount := 0
 		erasefailcount := 0
 		programfailcount2 := 0
@@ -140,20 +161,22 @@ func (c *SMARTCollector) collect(ch chan<- prometheus.Metric) (*prometheus.Desc,
 					break
 				}
 				v = disk.VendorSpecific[i+1]
-				log.Println("smart ", v)
 				if v != 0 {
 					log.Println("unexpected smart ", v)
 					continue
 				}
+				var i3, i6, i7, i8, i9, i10, i11, i12 uint8
 
-				i3 := disk.VendorSpecific[i+3]   // smart id
-				i6 := disk.VendorSpecific[i+6]   // actual normalized data
-				i7 := disk.VendorSpecific[i+7]   // worst normalized data
-				i8 := disk.VendorSpecific[i+8]   // raw value as decimal
-				i9 := disk.VendorSpecific[i+9]   // raw value as decimal
-				i10 := disk.VendorSpecific[i+10] // raw value as decimal
-				i11 := disk.VendorSpecific[i+11]
-				i12 := disk.VendorSpecific[i+12]
+				i3 = disk.VendorSpecific[i+3] // smart id
+				i6 = disk.VendorSpecific[i+6] // actual normalized data
+				i7 = disk.VendorSpecific[i+7] // worst normalized data
+				if len(disk.VendorSpecific) >= i+12 {
+					i8 = disk.VendorSpecific[i+8]   // raw value as decimal
+					i9 = disk.VendorSpecific[i+9]   // raw value as decimal
+					i10 = disk.VendorSpecific[i+10] // raw value as decimal
+					i11 = disk.VendorSpecific[i+11]
+					i12 = disk.VendorSpecific[i+12]
+				}
 
 				vendec := 0
 				// attributes may have different ways of calculation
@@ -202,7 +225,6 @@ func (c *SMARTCollector) collect(ch chan<- prometheus.Metric) (*prometheus.Desc,
 				case 7:
 					// fujitsu seems to use less fields here
 					// The raw value has different structure for different vendors and is often not meaningful as a decimal number.
-					seekerror = vendec
 					if i6 <= 60 || i7 <= 60 {
 						attrcriterror = attrcriterror + 1
 					}
@@ -313,8 +335,8 @@ func (c *SMARTCollector) collect(ch chan<- prometheus.Metric) (*prometheus.Desc,
 				case 194:
 					// temperature stores value only in one field
 					temperature = vendec
-					tempmax = int(i12)
 					tempmin = int(i10)
+					tempmax = int(i12)
 				case 196:
 					// critical, fujitsu uses other fields for something else, so dont use all fields together
 					// many crucial m500 use 16 as raw value. ssd have increasing values over time
@@ -400,6 +422,103 @@ func (c *SMARTCollector) collect(ch chan<- prometheus.Metric) (*prometheus.Desc,
 			}
 		}
 
+		// calculate health with restrict option out of: id1 (weight 2),id5 (weight 6),10 (weight 6),196 (weight 4),197 (weight 4),198 (weight6)
+		// standard calculation: id1 (weight 0,5), id5 (weight 1), 10 (weight 3), 196 (weight 0,6), 197 (weight 0,6), 198 (weight 1)
+		if lifetimeremain != 0 { // dirty way of how to detect hdd
+			health := 0
+			if hddhealthcalc == "restricted" {
+				health = 100*(100-reallocatedsectors*6)*
+					(100-rawreaderror*2)*
+					(100-spinretry*6)*
+					(100-reallocationevent*4)*
+					(100-pendingsectors*4)*
+					(100-uncorrectablesectors*6)/10 ^ 12
+			} else {
+				health = int(100*(100-float64(reallocatedsectors)*1)*
+					(100-float64(rawreaderror)*0.5)*
+					(100-float64(spinretry)*3)*
+					(100-float64(reallocationevent)*0.6)*
+					(100-float64(pendingsectors)*0.6)*
+					(100-float64(uncorrectablesectors)*1)/10) ^ 12
+			}
+
+			if int(health) <= 99 {
+				log.Println("Critical: HDD Device health is", health, "%.")
+				healtherror = healtherror + 1
+			} else {
+				log.Println("HDD Device health is", health, "%.")
+			}
+		}
+
+		// Calculate SSD health based on remaining sectors id170,id180
+		if lifetimeremain != 0 { // dirty way of how to detect ssd
+			if reserveblocks > 0 && reallocatedsectors >= 0 {
+				remainblocksperc := 100 * reserveblocks / (reserveblocks + reallocatedsectors)
+				if remainblocksperc <= 10 {
+					log.Println("Critical: SSD remaining reserve blocks", remainblocksperc, "%.")
+					healtherror = healtherror + 1
+				} else {
+					log.Println("SSD remaining reserve blocks", remainblocksperc, "%.")
+				}
+			}
+		}
+
+		// Print if there were critical smart attributes
+		if lifetimeremain != 0 { // detect ssd
+			if attrcriterror > 0 {
+				log.Println("Critical: Device is reporting a problem on Smart Attribute(s).")
+				sumattrcriterror = sumattrcriterror + 1
+			}
+		} else {
+			if attrcriterror > 0 || hddcriterror > 0 {
+				log.Println("Critical: Device is reporting a problem on Smart Attribute(s).")
+				sumattrcriterror = sumattrcriterror + 1
+			}
+		}
+
+		// check if disk is of old age
+		if poh > DiskOldAge {
+			log.Println("Warning: Old age", poh, "/", DiskOldAge, "(please verify, some vendors use minutes or seconds instead hours).")
+			agewarnerror = agewarnerror + 1
+		}
+
+		// display written GiB for SSDs
+		if lbawrite > 0 {
+			if strings.Contains(disk.InstanceName, "Intel") {
+				lbawritecalc := lbawrite * 32 / 1024
+				log.Println("Writes to Disk", lbawritecalc, "GiB (32MiB units).")
+			} else {
+				lbawritecalc := lbawrite * 512 / (1024 ^ 3)
+				log.Println("Writes to Disk", lbawritecalc, "GiB (512 byte sectors).")
+			}
+		}
+
+		// check if temperature is ok
+		if tempcritarg != 0 && temperature > tempcritarg {
+			log.Println("Critical: Temperature", temperature, "C is above critical limit of ", tempcritarg, "C. (Max/Min ", tempmax, "/", tempmin, ")")
+			tempcriterror = tempcriterror + 1
+		} else if tempwarnarg != 0 && temperature > tempwarnarg {
+			log.Println("Warning: Temperature", temperature, "C is above warning limit of", tempwarnarg, "C. (Max/Min ", tempmax, "/", tempmin, ")")
+			tempwarnerror = tempwarnerror + 1
+		} else if tempwarnarg != 0 || tempcritarg != 0 {
+			// if limits were given but there is no alarm
+			log.Println("Temperature", temperature, "C is within bounds. (Max/Min ", tempmax, "/", tempmin, ")")
+		} else {
+			// if no limits given, just show temperature
+			log.Println("Temperature is", temperature, "C. (Max/Min ", tempmax, "/", tempmin, ")")
+		}
+
+		// display average spin time
+		if spinavg > 0 {
+			log.Println("Average spin time is", spinavg, "ms.")
+		}
+
+		// XXX trigger alarms with prometheus?
+		if predicterror > 0 || tempcriterror > 0 || sumattrcriterror > 0 || healtherror > 0 {
+			log.Println("CRITICAL (# of discs): Predicted Errors", predicterror, "Health Errors", healtherror, "Attribute Errors", sumattrcriterror, "Temp Errors", tempcriterror)
+		} else if tempwarnerror > 0 || agewarnerror > 0 {
+			log.Println("WARNING (# of discs): Temp Errors", tempwarnerror, "Age Errors", agewarnerror)
+		}
 	}
 
 	return nil, nil
