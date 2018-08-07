c156f2bcbe81c31c1318b37e923fbfb14e75ddbb
Steve Zook <szook@webstaurantstore.com>
2018-08-07 15:42:16 -0400

    protect against emtpy wmi query result sets
    If an WMI query were to return an empty result set, trying to access the
    first element in the result set would result in a `panic: runtime error:
    index out of range` error.
    
    add logic to explicitly check if the result set size is 0 and return an
    error rather.
    
    Fixes https://github.com/martinlindhe/wmi_exporter/issues/240

--- collector/ad.go
+++ collector/ad.go
@@ -3,6 +3,8 @@
 package collector
 
 import (
+	"errors"
+
 	"github.com/StackExchange/wmi"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/common/log"
@@ -615,6 +617,9 @@ func (c *ADCollector) collect(ch chan<- prometheus.Metric) (*prometheus.Desc, er
 	q := queryAll(&dst)
 	if err := wmi.Query(q, &dst); err != nil {
 		return nil, err
+	}
+	if len(dst) == 0 {
+		return nil, errors.New("WMI query returned empty result set")
 	}
 
 	ch <- prometheus.MustNewConstMetric(
