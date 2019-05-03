9c51c3530512ef0f9c1d111430228f00855d3a51
Matas Tvarijonas <matas.tvarijonas@adform.com>
2019-05-03 18:04:46 +0300

    cleanup

--- collector/cpu.go
+++ collector/cpu.go
@@ -38,7 +38,7 @@ func getWindowsVersion() float64 {
 	}
 
 	currentv_flt, err := strconv.ParseFloat(currentv, 64)
-	//	log.Info(currentv_flt)
+
 	log.Debugf("Detected Windows version %d.%d\n", currentv_flt)
 
 	return currentv_flt
