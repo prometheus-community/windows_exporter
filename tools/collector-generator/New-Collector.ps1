6322bb124f66db4b5bca7c0662d77819239eb119
Поляков Игорь <bitshift.it@icloud.com>
2019-08-01 18:00:42 +0300

    Undo ps1 changes

--- tools/collector-generator/New-Collector.ps1
+++ tools/collector-generator/New-Collector.ps1
@@ -17,7 +17,7 @@ else {
     $wmiObject = Get-WMIObject -ComputerName $ComputerName -Class $Class
 }
 
-$members = @($wmiObject `
+$members = $wmiObject `
     | Get-Member -MemberType Properties `
     | Where-Object { $_.Definition -Match '^u?int' -and $_.Name -NotMatch '_' } `
     | Select-Object Name, @{Name="Type";Expression={$_.Definition.Split(" ")[0]}}
@@ -25,7 +25,7 @@ $input = @{
     "Class"=$Class;
     "CollectorName"=$CollectorName;
     "Members"=$members
-} | ConvertTo-Json)
+} | ConvertTo-Json
 $outFileName = "..\..\collector\$CollectorName.go".ToLower()
 $input | .\collector-generator.exe | Out-File -NoClobber -Encoding UTF8 $outFileName
 go fmt $outFileName
