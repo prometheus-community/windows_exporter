3a96ba9772ce27afa090412f782b47beca031340
Ben Reedy <breed808@breed808.com>
2020-10-29 17:33:54 +1000

    Add unit test for config flattening functions
    Signed-off-by: Ben Reedy <breed808@breed808.com>

---
+++ config/flatten_test.go
@@ -0,0 +1,33 @@
+package config
+
+import (
+	"gopkg.in/yaml.v2"
+	"reflect"
+	"testing"
+)
+
+// Unmarshal good configuration file and confirm data is flattened correctly
+func TestConfigFlattening(t *testing.T) {
+	goodYamlConfig := []byte(`---
+
+    collectors:
+      enabled: cpu,net,service
+
+    log:
+      level: debug`)
+	var data map[string]interface{}
+	err := yaml.Unmarshal(goodYamlConfig, &data)
+	if err != nil {
+		t.Error(err)
+	}
+
+	expectedResult := map[string]string{
+		"collectors.enabled": "cpu,net,service",
+		"log.level":          "debug",
+	}
+	flattenedValues := flatten(data)
+
+	if !reflect.DeepEqual(expectedResult, flattenedValues) {
+		t.Errorf("Flattened values do not match!\nExpected result: %s\nActual result: %s", expectedResult, flattenedValues)
+	}
+}
