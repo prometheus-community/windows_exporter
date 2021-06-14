8e2b346561e81027cd657e7a599548fe7f06dfe0
Rob Scheepens <rob.scheepens@nutanix.com>
2021-06-14 09:48:42 +0200

    exporter.go: Added physical_disk to defaultCollectors, add to e2e tests
    Signed-off-by: Rob Scheepens <rob.scheepens@nutanix.com>
    Signed-off-by: Brantley West <brantley@nutanix.com>

--- tools/end-to-end-test.ps1
+++ tools/end-to-end-test.ps1
@@ -18,7 +18,7 @@ mkdir $textfile_dir | Out-Null
 Copy-Item 'e2e-textfile.prom' -Destination "$($textfile_dir)/e2e-textfile.prom"
 
 # Omit dynamic collector information that will change after each run
-$skip_re = "^(go_|windows_exporter_build_info|windows_exporter_collector_duration_seconds|windows_exporter_perflib_snapshot_duration_seconds|process_|windows_textfile_mtime_seconds|windows_cpu|windows_cs|windows_logical_disk|windows_net|windows_os|windows_service|windows_system|windows_textfile_mtime_seconds)"
+$skip_re = "^(go_|windows_exporter_build_info|windows_exporter_collector_duration_seconds|windows_exporter_perflib_snapshot_duration_seconds|process_|windows_textfile_mtime_seconds|windows_cpu|windows_cs|windows_logical_disk|windows_physical_disk|windows_net|windows_os|windows_service|windows_system|windows_textfile_mtime_seconds)"
 
 # Start process in background, awaiting HTTP requests.
 # Use default collectors, port and address: http://localhost:9182/metrics
