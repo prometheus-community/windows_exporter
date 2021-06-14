2ca07fd12af7eeda97f786d07348b4157ea7c738
Rob Scheepens <rob.scheepens@nutanix.com>
2021-06-14 09:48:42 +0200

    exporter.go: Added physical_disk to defaultCollectors
    Signed-off-by: Rob Scheepens <rob.scheepens@nutanix.com>
    Signed-off-by: Brantley West <brantley@nutanix.com>

--- exporter.go
+++ exporter.go
@@ -44,7 +44,7 @@ type prometheusVersion struct {
 }
 
 const (
-	defaultCollectors            = "cpu,cs,logical_disk,net,os,service,system,textfile"
+	defaultCollectors            = "cpu,cs,logical_disk,physical_disk,net,os,service,system,textfile"
 	defaultCollectorsPlaceholder = "[defaults]"
 )
 
