a4224280d180be53693f01c80c171dd8ff13999c
Austin D. Krauza <krauza.austin@gmail.com>
2021-12-28 18:10:06 -0500

    Reformat ADFS collector using gofmt
    Signed-off-by: Austin D. Krauza <krauza.austin@gmail.com>

--- collector/adfs.go
+++ collector/adfs.go
@@ -324,49 +324,49 @@ func newADFSCollector() (Collector, error) {
 }
 
 type perflibADFS struct {
-	AdLoginConnectionFailures             float64 `perflib:"AD Login Connection Failures"`
-	CertificateAuthentications            float64 `perflib:"Certificate Authentications"`
-	DeviceAuthentications                 float64 `perflib:"Device Authentications"`
-	ExtranetAccountLockouts               float64 `perflib:"Extranet Account Lockouts"`
-	FederatedAuthentications              float64 `perflib:"Federated Authentications"`
-	PassportAuthentications               float64 `perflib:"Microsoft Passport Authentications"`
-	PassiveRequests                       float64 `perflib:"Passive Requests"`
-	PasswordChangeFailed                  float64 `perflib:"Password Change Failed Requests"`
-	PasswordChangeSucceeded               float64 `perflib:"Password Change Successful Requests"`
-	TokenRequests                         float64 `perflib:"Token Requests"`
-	WindowsIntegratedAuthentications      float64 `perflib:"Windows Integrated Authentications"`
-	OAuthAuthZRequests                    float64 `perflib:"OAuth AuthZ Requests"`
-	OAuthClientAuthentications            float64 `perflib:"OAuth Client Authentications"`
-	OAuthClientAuthenticationFailures     float64 `perflib:"OAuth Client Authentications Failures"`
-	OAuthClientCredentialRequestFailures  float64 `perflib:"OAuth Client Credentials Request Failures"`
-	OAuthClientCredentialRequests         float64 `perflib:"OAuth Client Credentials Requests"`
-	OAuthClientPrivKeyJWTAuthnFailures    float64 `perflib:"OAuth Client Private Key Jwt Authentication Failures"`
-	OAuthClientPrivKeyJWTAuthentications  float64 `perflib:"OAuth Client Private Key Jwt Authentications"`
-	OAuthClientBasicAuthnFailures         float64 `perflib:"OAuth Client Secret Basic Authentication Failures"`
-	OAuthClientBasicAuthentications       float64 `perflib:"OAuth Client Secret Basic Authentication Requests"`
-	OAuthClientSecretPostAuthnFailures    float64 `perflib:"OAuth Client Secret Post Authentication Failures"`
-	OAuthClientSecretPostAuthentications  float64 `perflib:"OAuth Client Secret Post Authentications"`
-	OAuthClientWindowsAuthnFailures       float64 `perflib:"OAuth Client Windows Integrated Authentication Failures"`
-	OAuthClientWindowsAuthentications     float64 `perflib:"OAuth Client Windows Integrated Authentications"`
-	OAuthLogonCertRequestFailures         float64 `perflib:"OAuth Logon Certificate Request Failures"`
-	OAuthLogonCertTokenRequests           float64 `perflib:"OAuth Logon Certificate Token Requests"`
-	OAuthPasswordGrantRequestFailures     float64 `perflib:"OAuth Password Grant Request Failures"`
-	OAuthPasswordGrantRequests            float64 `perflib:"OAuth Password Grant Requests"`
-	OAuthTokenRequests                    float64 `perflib:"OAuth Token Requests"`
-	SAMLPTokenRequests                    float64 `perflib:"SAML-P Token Requests"`
-	SSOAuthenticationFailures             float64 `perflib:"SSO Authentication Failures"`
-	SSOAuthentications                    float64 `perflib:"SSO Authentications"`
-	WSFedTokenRequests                    float64 `perflib:"WS-Fed Token Requests"`
-	WSTrustTokenRequests                  float64 `perflib:"WS-Trust Token Requests"`
-	UsernamePasswordAuthnFailures         float64 `perflib:"U/P Authentication Failures"`
-	UsernamePasswordAuthentications       float64 `perflib:"U/P Authentications"`
-	ExternalAuthentications               float64 `perflib:"External Authentications"`
-	ExternalAuthNFailures                 float64 `perflib:"External Authentication Failures"`
-	ArtifactDBFailures                    float64 `perflib:"Artifact Database Connection Failures"`
-	AvgArtifactDBQueryTime                float64 `perflib:"Average Artifact Database Query Time"`
-	ConfigDBFailures                      float64 `perflib:"Configuration Database Connection Failures"`
-	AvgConfigDBQueryTime                  float64 `perflib:"Average Config Database Query Time"`
-	FederationMetadataRequests            float64 `perflib:"Federation Metadata Requests"`
+	AdLoginConnectionFailures            float64 `perflib:"AD Login Connection Failures"`
+	CertificateAuthentications           float64 `perflib:"Certificate Authentications"`
+	DeviceAuthentications                float64 `perflib:"Device Authentications"`
+	ExtranetAccountLockouts              float64 `perflib:"Extranet Account Lockouts"`
+	FederatedAuthentications             float64 `perflib:"Federated Authentications"`
+	PassportAuthentications              float64 `perflib:"Microsoft Passport Authentications"`
+	PassiveRequests                      float64 `perflib:"Passive Requests"`
+	PasswordChangeFailed                 float64 `perflib:"Password Change Failed Requests"`
+	PasswordChangeSucceeded              float64 `perflib:"Password Change Successful Requests"`
+	TokenRequests                        float64 `perflib:"Token Requests"`
+	WindowsIntegratedAuthentications     float64 `perflib:"Windows Integrated Authentications"`
+	OAuthAuthZRequests                   float64 `perflib:"OAuth AuthZ Requests"`
+	OAuthClientAuthentications           float64 `perflib:"OAuth Client Authentications"`
+	OAuthClientAuthenticationFailures    float64 `perflib:"OAuth Client Authentications Failures"`
+	OAuthClientCredentialRequestFailures float64 `perflib:"OAuth Client Credentials Request Failures"`
+	OAuthClientCredentialRequests        float64 `perflib:"OAuth Client Credentials Requests"`
+	OAuthClientPrivKeyJWTAuthnFailures   float64 `perflib:"OAuth Client Private Key Jwt Authentication Failures"`
+	OAuthClientPrivKeyJWTAuthentications float64 `perflib:"OAuth Client Private Key Jwt Authentications"`
+	OAuthClientBasicAuthnFailures        float64 `perflib:"OAuth Client Secret Basic Authentication Failures"`
+	OAuthClientBasicAuthentications      float64 `perflib:"OAuth Client Secret Basic Authentication Requests"`
+	OAuthClientSecretPostAuthnFailures   float64 `perflib:"OAuth Client Secret Post Authentication Failures"`
+	OAuthClientSecretPostAuthentications float64 `perflib:"OAuth Client Secret Post Authentications"`
+	OAuthClientWindowsAuthnFailures      float64 `perflib:"OAuth Client Windows Integrated Authentication Failures"`
+	OAuthClientWindowsAuthentications    float64 `perflib:"OAuth Client Windows Integrated Authentications"`
+	OAuthLogonCertRequestFailures        float64 `perflib:"OAuth Logon Certificate Request Failures"`
+	OAuthLogonCertTokenRequests          float64 `perflib:"OAuth Logon Certificate Token Requests"`
+	OAuthPasswordGrantRequestFailures    float64 `perflib:"OAuth Password Grant Request Failures"`
+	OAuthPasswordGrantRequests           float64 `perflib:"OAuth Password Grant Requests"`
+	OAuthTokenRequests                   float64 `perflib:"OAuth Token Requests"`
+	SAMLPTokenRequests                   float64 `perflib:"SAML-P Token Requests"`
+	SSOAuthenticationFailures            float64 `perflib:"SSO Authentication Failures"`
+	SSOAuthentications                   float64 `perflib:"SSO Authentications"`
+	WSFedTokenRequests                   float64 `perflib:"WS-Fed Token Requests"`
+	WSTrustTokenRequests                 float64 `perflib:"WS-Trust Token Requests"`
+	UsernamePasswordAuthnFailures        float64 `perflib:"U/P Authentication Failures"`
+	UsernamePasswordAuthentications      float64 `perflib:"U/P Authentications"`
+	ExternalAuthentications              float64 `perflib:"External Authentications"`
+	ExternalAuthNFailures                float64 `perflib:"External Authentication Failures"`
+	ArtifactDBFailures                   float64 `perflib:"Artifact Database Connection Failures"`
+	AvgArtifactDBQueryTime               float64 `perflib:"Average Artifact Database Query Time"`
+	ConfigDBFailures                     float64 `perflib:"Configuration Database Connection Failures"`
+	AvgConfigDBQueryTime                 float64 `perflib:"Average Config Database Query Time"`
+	FederationMetadataRequests           float64 `perflib:"Federation Metadata Requests"`
 }
 
 func (c *adfsCollector) Collect(ctx *ScrapeContext, ch chan<- prometheus.Metric) error {
@@ -613,7 +613,7 @@ func (c *adfsCollector) Collect(ctx *ScrapeContext, ch chan<- prometheus.Metric)
 	ch <- prometheus.MustNewConstMetric(
 		c.avgArtifactDBQueryTime,
 		prometheus.GaugeValue,
-		adfsData[0].AvgArtifactDBQueryTime  * math.Pow(10, -8),
+		adfsData[0].AvgArtifactDBQueryTime*math.Pow(10, -8),
 	)
 
 	ch <- prometheus.MustNewConstMetric(
@@ -625,7 +625,7 @@ func (c *adfsCollector) Collect(ctx *ScrapeContext, ch chan<- prometheus.Metric)
 	ch <- prometheus.MustNewConstMetric(
 		c.avgConfigDBQueryTime,
 		prometheus.GaugeValue,
-		adfsData[0].AvgConfigDBQueryTime * math.Pow(10, -8),
+		adfsData[0].AvgConfigDBQueryTime*math.Pow(10, -8),
 	)
 
 	ch <- prometheus.MustNewConstMetric(
