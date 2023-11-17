f1b2b319f56fd1048ec8b6f8b3edd476ddb3b694
Ben Reedy <breed808@breed808.com>
2023-11-18 06:53:54 +1000

    feat: Update WiX from v3 to v4
    This change updates WiX to the latest major upstream version, which is a
    prerequisite for ARM64 MSI builds.
    
    Signed-off-by: Ben Reedy <breed808@breed808.com>

--- installer/build.ps1
+++ installer/build.ps1
@@ -19,42 +19,13 @@ Trap {
     Pop-Location
 }
 
-if ($PSVersionTable.PSVersion.Major -lt 5) {
-    Write-Error "Powershell version 5 required"
-    exit 1
-}
-
-$wc = New-Object System.Net.WebClient
-function Get-FileIfNotExists {
-    Param (
-        $Url,
-        $Destination
-    )
-    if (-not (Test-Path $Destination)) {
-        Write-Verbose "Downloading $Url"
-        $wc.DownloadFile($Url, $Destination)
-    }
-    else {
-        Write-Verbose "${Destination} already exists. Skipping."
-    }
-}
-
-$sourceDir = mkdir -Force Source
-mkdir -Force Work, Output | Out-Null
-
-Write-Verbose "Downloading WiX..."
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-Get-FileIfNotExists "https://github.com/wixtoolset/wix3/releases/download/wix311rtm/wix311-binaries.zip" "$sourceDir\wix-binaries.zip"
-mkdir -Force WiX | Out-Null
-Expand-Archive -Path "${sourceDir}\wix-binaries.zip" -DestinationPath WiX -Force
-
+mkdir -Force Work | Out-Null
 Copy-Item -Force $PathToExecutable Work/windows_exporter.exe
 
 Write-Verbose "Creating windows_exporter-${Version}-${Arch}.msi"
 $wixArch = @{"amd64" = "x64"; "386" = "x86"}[$Arch]
 $wixOpts = "-ext WixFirewallExtension -ext WixUtilExtension"
-Invoke-Expression "WiX\candle.exe -nologo -arch $wixArch $wixOpts -out Work\windows_exporter.wixobj -dVersion=`"$Version`" windows_exporter.wxs"
-Invoke-Expression "WiX\light.exe -nologo -spdb $wixOpts -out `"Output\windows_exporter-${Version}-${Arch}.msi`" Work\windows_exporter.wixobj"
+Invoke-Expression "wix build -arch $wixArch -o .\windows_exporter-$($Version)-$($Arch).msi .\windows_exporter.wxs -d Version=$($Version) -ext WixToolset.Firewall.wixext -ext WixToolset.Util.wixext"
 
 Write-Verbose "Done!"
 Pop-Location
