d4b48e52e6d2dc4d02c3c35ce8057cbb8d23e1d2
Ben Reedy <breed808@breed808.com>
2023-11-19 13:29:47 +1000

    feat: Add ARM64 MSI builds
    Signed-off-by: Ben Reedy <breed808@breed808.com>

--- installer/build.ps1
+++ installer/build.ps1
@@ -5,7 +5,7 @@ Param (
     [Parameter(Mandatory = $true)]
     [String] $Version,
     [Parameter(Mandatory = $false)]
-    [ValidateSet("amd64", "386")]
+    [ValidateSet("amd64", "arm64", "386")]
     [String] $Arch = "amd64"
 )
 $ErrorActionPreference = "Stop"
@@ -23,7 +23,7 @@ mkdir -Force Work | Out-Null
 Copy-Item -Force $PathToExecutable Work/windows_exporter.exe
 
 Write-Verbose "Creating windows_exporter-${Version}-${Arch}.msi"
-$wixArch = @{"amd64" = "x64"; "386" = "x86"}[$Arch]
+$wixArch = @{"amd64" = "x64"; "arm64" = "arm64"; "386" = "x86"}[$Arch]
 $wixOpts = "-ext WixFirewallExtension -ext WixUtilExtension"
 Invoke-Expression "wix build -arch $wixArch -o .\windows_exporter-$($Version)-$($Arch).msi .\windows_exporter.wxs -d Version=$($Version) -ext WixToolset.Firewall.wixext -ext WixToolset.Util.wixext"
 
