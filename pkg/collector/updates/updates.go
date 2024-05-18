93a74153e954709cfe1ce934187caafffb2d14c6
Jan-Otto Kröpke <mail@jkroepke.de>
2024-05-18 22:48:57 +0000

    wip
    Signed-off-by: Jan-Otto Kröpke <mail@jkroepke.de>

--- pkg/collector/updates/updates.go
+++ pkg/collector/updates/updates.go
@@ -5,15 +5,15 @@ package updates
 import (
 	"errors"
 	"fmt"
+	"runtime"
+	"time"
+
 	"github.com/alecthomas/kingpin/v2"
 	"github.com/go-kit/log"
 	"github.com/go-kit/log/level"
 	"github.com/go-ole/go-ole"
 	"github.com/go-ole/go-ole/oleutil"
 	"github.com/prometheus/client_golang/prometheus"
-	"regexp"
-	"runtime"
-	"time"
 
 	"github.com/prometheus-community/windows_exporter/pkg/types"
 )
@@ -21,46 +21,39 @@ import (
 const (
 	Name = "updates"
 
-	FlagUpdatesInclude = "collector.updates.include"
-	FlagUpdatesExclude = "collector.updates.exclude"
+	FlagCacheDuration = "collector.updates.cache-duration"
 )
 
 type Config struct {
-	printerInclude string `yaml:"printer_include"`
-	printerExclude string `yaml:"printer_exclude"`
+	cacheDuration time.Duration `yaml:"cache_duration"`
 }
 
 var ConfigDefaults = Config{
-	printerInclude: ".+",
-	printerExclude: "",
+	cacheDuration: 24 * time.Hour,
 }
 
 type collector struct {
 	logger log.Logger
 
-	printerInclude *string
-	printerExclude *string
+	cacheDuration *time.Duration
+	lastScrape    time.Time
 
 	update *prometheus.Desc
-
-	printerIncludePattern *regexp.Regexp
-	printerExcludePattern *regexp.Regexp
 }
 
 type update struct {
-	title        string
-	updateStatus WindowsUpdateStatus
+	status  windowsUpdateStatus
+	seventy string
 }
 
-type updates []update
+type availableUpdateCount map[update]int
 
 func New(logger log.Logger, config *Config) types.Collector {
 	if config == nil {
 		config = &ConfigDefaults
 	}
 	c := &collector{
-		printerInclude: &config.printerInclude,
-		printerExclude: &config.printerExclude,
+		cacheDuration: &config.cacheDuration,
 	}
 	c.SetLogger(logger)
 	return c
@@ -68,14 +61,10 @@ func New(logger log.Logger, config *Config) types.Collector {
 
 func NewWithFlags(app *kingpin.Application) types.Collector {
 	c := &collector{
-		printerInclude: app.Flag(
-			FlagUpdatesInclude,
-			"Regular expression to match printers to collect metrics for",
-		).Default(ConfigDefaults.printerInclude).String(),
-		printerExclude: app.Flag(
-			FlagUpdatesExclude,
-			"Regular expression to match printers to exclude",
-		).Default(ConfigDefaults.printerExclude).String(),
+		cacheDuration: app.Flag(
+			FlagCacheDuration,
+			"How long should the Windows Update information be cached for.",
+		).Default(ConfigDefaults.cacheDuration.String()).Duration(),
 	}
 	return c
 }
@@ -88,7 +77,7 @@ func (c *collector) Build() error {
 	c.update = prometheus.NewDesc(
 		prometheus.BuildFQName(types.Namespace, Name, "update"),
 		"Windows Update information",
-		[]string{"severity", "kb"},
+		[]string{"severity"},
 		nil,
 	)
 
@@ -107,16 +96,20 @@ func (c *collector) Collect(_ *types.ScrapeContext, ch chan<- prometheus.Metric)
 		return err
 	}
 
-	for _, update := range updates {
-		severity := "unknown"
-		ch <- prometheus.MustNewConstMetric(
-			c.update,
-			prometheus.GaugeValue,
-			1,
-			severity,
-			update.title,
-		)
-	}
+	/*
+		for _, update := range updates {
+			severity := "unknown"
+			ch <- prometheus.MustNewConstMetric(
+				c.update,
+				prometheus.GaugeValue,
+				1,
+				severity,
+			)
+		}
+
+	*/
+
+	_ = updates
 
 	return nil
 }
@@ -124,7 +117,7 @@ func (c *collector) Collect(_ *types.ScrapeContext, ch chan<- prometheus.Metric)
 // S_FALSE is returned by CoInitialize if it was already called on this thread.
 const S_FALSE = 0x00000001
 
-type WindowsUpdateStatus int
+type windowsUpdateStatus int
 
 const (
 	WUStatusPending = iota
@@ -135,7 +128,7 @@ const (
 	WUStatusAborted
 )
 
-func (c *collector) getUpdates() (updates, error) {
+func (c *collector) getUpdates() (availableUpdateCount, error) {
 	// The only way to run WMI queries in parallel while being thread-safe is to
 	// ensure the CoInitialize[Ex]() call is bound to its current OS thread.
 	// Otherwise, attempting to initialize and run parallel queries across
@@ -164,7 +157,11 @@ func (c *collector) getUpdates() (updates, error) {
 		return nil, fmt.Errorf("IID_IDispatch: %w", err)
 	}
 	defer musQueryInterface.Release()
-	oleutil.PutProperty(musQueryInterface, "ClientApplicationID", "Cagent")
+
+	_, err = oleutil.PutProperty(musQueryInterface, "ClientApplicationID", "windows_exporter")
+	if err != nil {
+		return nil, fmt.Errorf("put ClientApplicationID: %w", err)
+	}
 
 	us, err := oleutil.CallMethod(musQueryInterface, "CreateUpdateSearcher")
 	if err != nil {
@@ -183,54 +180,33 @@ func (c *collector) getUpdates() (updates, error) {
 	usd := us.ToIDispatch()
 	defer usd.Release()
 
-	_, err = oleutil.PutProperty(usd, "Online", "false")
-
 	usr, err := oleutil.CallMethod(usd, "Search", "IsInstalled=0 and Type='Software' and IsHidden=0")
 	if err != nil {
-		return nil, fmt.Errorf("search for updates: %w", err)
+		return nil, fmt.Errorf("search updates: %w", err)
 	}
 
-	// Query the IDispatch interface of the object
 	usrd := usr.ToIDispatch()
 	defer usrd.Release()
 
-	upd, err2 := oleutil.GetProperty(usrd, "Updates")
-	if err2 != nil {
-		return nil, fmt.Errorf("get updates: %w", err2)
+	upd, err := oleutil.GetProperty(usrd, "Updates")
+	if err != nil {
+		return nil, fmt.Errorf("get updates: %w", err)
 	}
 
 	updd := upd.ToIDispatch()
 	defer updd.Release()
 
-	thc, err2 := oleutil.CallMethod(usd, "GetTotalHistoryCount")
-	if err2 != nil {
-		return nil, fmt.Errorf("get total history count: %w", err2)
+	countUpdd, err := oleutil.GetProperty(updd, "Count")
+	if err != nil {
+		return nil, fmt.Errorf("get count: %w", err)
 	}
 
-	thcn := int(thc.Val)
-
-	// uhistRaw is list of update event records on the computer in descending chronological order
-	uhistRaw, err2 := oleutil.CallMethod(usd, "QueryHistory", 0, thcn)
-	if err2 != nil {
-		return nil, fmt.Errorf("query history: %w", err2)
-	}
-
-	uhist := uhistRaw.ToIDispatch()
-	defer uhist.Release()
-
-	countUhist, err2 := oleutil.GetProperty(uhist, "Count")
-	if err2 != nil {
-		return nil, fmt.Errorf("get count: %w", err2)
-	}
-
-	availableUpdates := make(updates, 0, countUhist.Val)
-	var lastTimeUpdated time.Time
-
-	for i := 0; i < int(countUhist.Val); i++ {
+	availableUpdates := availableUpdateCount{}
+	for i := 0; i < int(countUpdd.Val); i++ {
 		// other available properties can be found here:
-		// https://docs.microsoft.com/en-us/previous-versions/windows/desktop/aa386472(v%3dvs.85)
+		// https://learn.microsoft.com/en-us/previous-versions/windows/desktop/aa386114(v=vs.85)
 
-		itemRaw, err := oleutil.GetProperty(uhist, "Item", i)
+		itemRaw, err := oleutil.GetProperty(updd, "Item", i)
 		if err != nil {
 			_ = level.Error(c.logger).Log("msg", "failed to fetch Windows Update history item", "err", err)
 			continue
@@ -239,47 +215,25 @@ func (c *collector) getUpdates() (updates, error) {
 		item := itemRaw.ToIDispatch()
 		defer item.Release()
 
-		resultCode, err := oleutil.GetProperty(item, "ResultCode")
+		updateType, err := oleutil.GetProperty(item, "Type")
 		if err != nil {
-			// On Win10 machine returns "Exception occurred." after 75 updates so it looks like some undocumented internal limit.
-			// We only need the last ones to found "Pending" updates so just ignore this error
+			_ = level.Error(c.logger).Log("msg", "failed to fetch Windows Update history item type", "err", err)
 			continue
 		}
 
-		updateIdentity, err := oleutil.GetProperty(item, "UpdateIdentity")
+		severity, err := oleutil.GetProperty(item, "MsrcSeverity")
 		if err != nil {
-			_ = level.Error(c.logger).Log("msg", "failed to fetch Windows Update history item result code", "err", err)
+			_ = level.Error(c.logger).Log("msg", "failed to fetch Windows Update history item severity", "err", err)
 			continue
 		}
-		fmt.Println(updateIdentity.ToString())
-		fmt.Println(int(resultCode.Val))
-		updateStatus := WindowsUpdateStatus(int(resultCode.Val))
-		if updateStatus == WUStatusPending || updateStatus == WUStatusInProgress {
-			updateIdentity, err := oleutil.GetProperty(item, "Title")
-			if err != nil {
-				_ = level.Error(c.logger).Log("msg", "failed to fetch Windows Update history item result code", "err", err)
-				continue
-			}
 
-			availableUpdates = append(availableUpdates, update{
-				title: updateIdentity.ToString(),
-
-				updateStatus: updateStatus,
-			})
+		categories, err := oleutil.GetProperty(item, "Categories")
+		if err != nil {
+			_ = level.Error(c.logger).Log("msg", "failed to fetch Windows Update history item categories", "err", err)
+			continue
 		}
 
-		if updateStatus == WUStatusCompleted {
-			date, err := oleutil.GetProperty(item, "Date")
-			if err != nil {
-				_ = level.Error(c.logger).Log("msg", "failed to fetch Windows Update history item date", "err", err)
-				continue
-			}
-			if updateDate, ok := date.Value().(time.Time); ok {
-				if lastTimeUpdated.IsZero() || updateDate.After(lastTimeUpdated) {
-					lastTimeUpdated = updateDate
-				}
-			}
-		}
+		_, _, _ = updateType, severity, categories
 	}
 
 	return availableUpdates, nil
