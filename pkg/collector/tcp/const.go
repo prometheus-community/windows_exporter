38c6b632978513bb0fdb7f7de271f79434dec101
Jan-Otto Kröpke <github@jkroepke.de>
2024-09-22 11:38:12 +0200

    use iphlpapi.dll to GetExtendedTcpTable
    Signed-off-by: astigmata <envoiici@gmail.com>

--- pkg/collector/tcp/const.go
+++ pkg/collector/tcp/const.go
@@ -3,6 +3,7 @@ package tcp
 // Win32_PerfRawData_Tcpip_TCPv4 docs
 // - https://msdn.microsoft.com/en-us/library/aa394341(v=vs.85).aspx
 // The TCPv6 performance object uses the same fields.
+// https://learn.microsoft.com/en-us/dotnet/api/system.net.networkinformation.tcpstate?view=net-8.0
 const (
 	ConnectionFailures          = "Connection Failures"
 	ConnectionsActive           = "Connections Active"
@@ -13,4 +14,19 @@ const (
 	SegmentsReceivedPersec      = "Segments Received/sec"
 	SegmentsRetransmittedPersec = "Segments Retransmitted/sec"
 	SegmentsSentPersec          = "Segments Sent/sec"
+
+    TCPTableClass               = 5
+
+	TCPStateClosed              = 1
+    TCPStateListening           = 2
+    TCPStateSynSent             = 3
+    TCPStateSynRcvd             = 4
+    TCPStateEstablished         = 5
+    TCPStateFinWait1            = 6
+    TCPStateFinWait2            = 7
+    TCPStateCloseWait           = 8
+    TCPStateClosing             = 9
+    TCPStateLastAck             = 10
+    TCPStateTimeWait            = 11
+    TCPStateDeleteTcb           = 12
 )
--- pkg/collector/tcp/tcp.go
+++ pkg/collector/tcp/tcp.go
@@ -7,15 +7,33 @@ import (
 	"log/slog"
 
 	"github.com/alecthomas/kingpin/v2"
+	"github.com/pkg/errors"
 	"github.com/prometheus-community/windows_exporter/pkg/perfdata"
 	"github.com/prometheus-community/windows_exporter/pkg/types"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/yusufpapurcu/wmi"
+	"syscall"
+	"unsafe"
 )
 
 const Name = "tcp"
 
 type Config struct{}
+
+var (
+	modiphlpapi             = syscall.NewLazyDLL("iphlpapi.dll")
+	procGetExtendedTcpTable = modiphlpapi.NewProc("GetExtendedTcpTable")
+)
+
+// MIB_TCPROW_OWNER_PID represents a row in the TCP connection table
+type MIB_TCPROW_OWNER_PID struct {
+	dwState      uint32
+	dwLocalAddr  uint32
+	dwLocalPort  uint32
+	dwRemoteAddr uint32
+	dwRemotePort uint32
+	dwOwningPid  uint32
+}
 
 var ConfigDefaults = Config{}
 
@@ -35,6 +53,37 @@ type Collector struct {
 	segmentsReceivedTotal      *prometheus.Desc
 	segmentsRetransmittedTotal *prometheus.Desc
 	segmentsSentTotal          *prometheus.Desc
+
+	connectionsCLOSED    *prometheus.Desc
+	connectionsLISTENING *prometheus.Desc
+	connectionsSYNSENT   *prometheus.Desc
+	connectionsSYNRCVD   *prometheus.Desc
+	connectionsFINWAIT1  *prometheus.Desc
+	connectionsFINWAIT2  *prometheus.Desc
+	connectionsCLOSEWAIT *prometheus.Desc
+	connectionsCLOSING   *prometheus.Desc
+	connectionsLASTACK   *prometheus.Desc
+	connectionsTIMEWAIT  *prometheus.Desc
+	connectionsDELETETCB *prometheus.Desc
+}
+
+func getExtendedTcpTable(pTCPTable uintptr, pdwSize *uint32, bOrder bool, ulAf uint32, tableClass uint32, reserved uint32) uintptr {
+	ret, _, _ := procGetExtendedTcpTable.Call(
+		pTCPTable,
+		uintptr(unsafe.Pointer(pdwSize)),
+		uintptr(boolToInt(bOrder)),
+		uintptr(ulAf),
+		uintptr(tableClass),
+		uintptr(reserved),
+	)
+	return ret
+}
+
+func boolToInt(b bool) int {
+	if b {
+		return 1
+	}
+	return 0
 }
 
 func New(config *Config) *Collector {
@@ -144,7 +193,72 @@ func (c *Collector) Build(_ *slog.Logger, _ *wmi.Client) error {
 		[]string{"af"},
 		nil,
 	)
-
+	c.connectionsCLOSED = prometheus.NewDesc(
+		prometheus.BuildFQName(types.Namespace, Name, "connections_closed"),
+		"Number of TCP connections in CLOSED state",
+		[]string{"af"},
+		nil,
+	)
+	c.connectionsLISTENING = prometheus.NewDesc(
+		prometheus.BuildFQName(types.Namespace, Name, "connections_listening"),
+		"Number of TCP connections in LISTENING state",
+		[]string{"af"},
+		nil,
+	)
+	c.connectionsSYNSENT = prometheus.NewDesc(
+		prometheus.BuildFQName(types.Namespace, Name, "connections_syn_sent"),
+		"Number of TCP connections in SYN_SENT state",
+		[]string{"af"},
+		nil,
+	)
+	c.connectionsSYNRCVD = prometheus.NewDesc(
+		prometheus.BuildFQName(types.Namespace, Name, "connections_syn_rcvd"),
+		"Number of TCP connections in SYN_RCVD state",
+		[]string{"af"},
+		nil,
+	)
+	c.connectionsFINWAIT1 = prometheus.NewDesc(
+		prometheus.BuildFQName(types.Namespace, Name, "connections_fin_wait_1"),
+		"Number of TCP connections in FIN_WAIT_1 state",
+		[]string{"af"},
+		nil,
+	)
+	c.connectionsFINWAIT2 = prometheus.NewDesc(
+		prometheus.BuildFQName(types.Namespace, Name, "connections_fin_wait_2"),
+		"Number of TCP connections in FIN_WAIT_2 state",
+		[]string{"af"},
+		nil,
+	)
+	c.connectionsCLOSEWAIT = prometheus.NewDesc(
+		prometheus.BuildFQName(types.Namespace, Name, "connections_close_wait"),
+		"Number of TCP connections in CLOSE_WAIT state",
+		[]string{"af"},
+		nil,
+	)
+	c.connectionsCLOSING = prometheus.NewDesc(
+		prometheus.BuildFQName(types.Namespace, Name, "connections_closing"),
+		"Number of TCP connections in CLOSING state",
+		[]string{"af"},
+		nil,
+	)
+	c.connectionsLASTACK = prometheus.NewDesc(
+		prometheus.BuildFQName(types.Namespace, Name, "connections_last_ack"),
+		"Number of TCP connections in LAST_ACK state",
+		[]string{"af"},
+		nil,
+	)
+	c.connectionsTIMEWAIT = prometheus.NewDesc(
+		prometheus.BuildFQName(types.Namespace, Name, "connections_time_wait"),
+		"Number of TCP connections in TIME_WAIT state",
+		[]string{"af"},
+		nil,
+	)
+	c.connectionsDELETETCB = prometheus.NewDesc(
+		prometheus.BuildFQName(types.Namespace, Name, "connections_delete_tcb"),
+		"Number of TCP connections in DELETE_TCB state",
+		[]string{"af"},
+		nil,
+	)
 	return nil
 }
 
@@ -235,5 +349,55 @@ func (c *Collector) collect(ch chan<- prometheus.Metric) error {
 
 	writeTCPCounters(data[perfdata.EmptyInstance], []string{"ipv6"}, c, ch)
 
+	stateCountsIPv4, err := getTCPConnectionStates(syscall.AF_INET)
+	if err != nil {
+		return fmt.Errorf("failed to collect TCP connection states for IPv4: %w", err)
+	}
+
+	stateCountsIPv6, err := getTCPConnectionStates(syscall.AF_INET6)
+	if err != nil {
+		return fmt.Errorf("failed to collect TCP connection states for IPv6: %w", err)
+	}
+
+	c.sendTCPStateMetrics(ch, stateCountsIPv4, "ipv4")
+	c.sendTCPStateMetrics(ch, stateCountsIPv6, "ipv6")
 	return nil
+}
+
+func (c *Collector) sendTCPStateMetrics(ch chan<- prometheus.Metric, stateCounts map[uint32]uint32, af string) {
+	ch <- prometheus.MustNewConstMetric(c.connectionsCLOSED, prometheus.GaugeValue, float64(stateCounts[TCPStateClosed]), af)
+	ch <- prometheus.MustNewConstMetric(c.connectionsLISTENING, prometheus.GaugeValue, float64(stateCounts[TCPStateListening]), af)
+	ch <- prometheus.MustNewConstMetric(c.connectionsSYNSENT, prometheus.GaugeValue, float64(stateCounts[TCPStateSynSent]), af)
+	ch <- prometheus.MustNewConstMetric(c.connectionsSYNRCVD, prometheus.GaugeValue, float64(stateCounts[TCPStateSynRcvd]), af)
+	ch <- prometheus.MustNewConstMetric(c.connectionsFINWAIT1, prometheus.GaugeValue, float64(stateCounts[TCPStateFinWait1]), af)
+	ch <- prometheus.MustNewConstMetric(c.connectionsFINWAIT2, prometheus.GaugeValue, float64(stateCounts[TCPStateFinWait2]), af)
+	ch <- prometheus.MustNewConstMetric(c.connectionsCLOSEWAIT, prometheus.GaugeValue, float64(stateCounts[TCPStateCloseWait]), af)
+	ch <- prometheus.MustNewConstMetric(c.connectionsCLOSING, prometheus.GaugeValue, float64(stateCounts[TCPStateClosing]), af)
+	ch <- prometheus.MustNewConstMetric(c.connectionsLASTACK, prometheus.GaugeValue, float64(stateCounts[TCPStateLastAck]), af)
+	ch <- prometheus.MustNewConstMetric(c.connectionsTIMEWAIT, prometheus.GaugeValue, float64(stateCounts[TCPStateTimeWait]), af)
+	ch <- prometheus.MustNewConstMetric(c.connectionsDELETETCB, prometheus.GaugeValue, float64(stateCounts[TCPStateDeleteTcb]), af)
+}
+
+func getTCPConnectionStates(family uint32) (map[uint32]uint32, error) {
+	var size uint32
+	stateCounts := make(map[uint32]uint32)
+
+	ret := getExtendedTcpTable(0, &size, true, family, TCPTableClass, 0)
+	if ret != 0 && ret != uintptr(syscall.ERROR_INSUFFICIENT_BUFFER) {
+		return nil, errors.Errorf("getExtendedTcpTable failed with code %d", ret)
+	}
+
+	buf := make([]byte, size)
+	ret = getExtendedTcpTable(uintptr(unsafe.Pointer(&buf[0])), &size, true, family, TCPTableClass, 0)
+	if ret != 0 {
+		return nil, errors.Errorf("getExtendedTcpTable failed with code %d", ret)
+	}
+
+	numEntries := *(*uint32)(unsafe.Pointer(&buf[0]))
+	for i := uint32(0); i < numEntries; i++ {
+		row := (*MIB_TCPROW_OWNER_PID)(unsafe.Pointer(&buf[4+i*uint32(unsafe.Sizeof(MIB_TCPROW_OWNER_PID{}))]))
+		stateCounts[row.dwState]++
+	}
+
+	return stateCounts, nil
 }
