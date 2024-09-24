d0b7b290fa704f06ee7f31336e56d5ba2c588ac2
Jan-Otto Kröpke <mail@jkroepke.de>
2024-09-24 22:30:06 +0200

    filetime: add collector
    Signed-off-by: Jan-Otto Kröpke <mail@jkroepke.de>

--- pkg/collector/filetime/filetime.go
+++ pkg/collector/filetime/filetime.go
@@ -3,34 +3,28 @@
 package filetime
 
 import (
-	"errors"
 	"fmt"
 	"log/slog"
 	"os"
 	"path/filepath"
-	"strconv"
 	"strings"
 	"sync"
-	"time"
 
 	"github.com/alecthomas/kingpin/v2"
 	"github.com/bmatcuk/doublestar/v4"
 	"github.com/prometheus-community/windows_exporter/pkg/types"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/yusufpapurcu/wmi"
-	"golang.org/x/sys/windows"
 )
 
 const Name = "filetime"
 
 type Config struct {
-	filePatterns      []string
-	EnableGlobPattern bool
+	filePatterns []string
 }
 
 var ConfigDefaults = Config{
-	filePatterns:      []string{},
-	EnableGlobPattern: false,
+	filePatterns: []string{},
 }
 
 // A Collector is a Prometheus Collector for collecting file times
@@ -67,19 +61,12 @@ func NewWithFlags(app *kingpin.Application) *Collector {
 
 	app.Flag(
 		"collectors.filetime.file-patterns",
-		"Comma-separated list of file paths. The file paths can include wildcard characters, for example, an asterisk (*) or a question mark (?).",
+		"Comma-separated list of file patterns. Each pattern is a glob pattern that can contain `*`, `?`, and `**` (recursive). See https://github.com/bmatcuk/doublestar#patterns",
 	).Default(strings.Join(ConfigDefaults.filePatterns, ",")).StringVar(&filePatterns)
 
-	app.Flag(
-		"collectors.filetime.enable-glob-pattern",
-		"Enable glob syntax in collectors.filetime.filepaths. This supports the standard glob syntax, including recursive pattern, but access time is not supported.",
-	).Default(strconv.FormatBool(c.config.EnableGlobPattern)).BoolVar(&c.config.EnableGlobPattern)
-
 	app.Action(func(*kingpin.ParseContext) error {
-		if c.config.EnableGlobPattern {
-			filePatterns = strings.ReplaceAll(filePatterns, "\\", "/")
-		}
-
+		// doublestar.Glob() requires forward slashes
+		filePatterns = strings.ReplaceAll(filePatterns, "\\", "/")
 		c.config.filePatterns = strings.Split(filePatterns, ",")
 
 		return nil
@@ -114,27 +101,12 @@ func (c *Collector) Build(_ *slog.Logger, _ *wmi.Client) error {
 		nil,
 	)
 
-	if c.config.EnableGlobPattern {
-		for _, filePattern := range c.config.filePatterns {
-			basePath, pattern := doublestar.SplitPattern(filePattern)
+	for _, filePattern := range c.config.filePatterns {
+		basePath, pattern := doublestar.SplitPattern(filePattern)
 
-			_, err := doublestar.Glob(os.DirFS(basePath), pattern, doublestar.WithFilesOnly())
-			if err != nil {
-				return fmt.Errorf("invalid glob pattern: %w", err)
-			}
-		}
-	} else {
-		var data windows.Win32finddata
-
-		for _, filePattern := range c.config.filePatterns {
-			firstFile, err := windows.FindFirstFile(windows.StringToUTF16Ptr(filePattern), &data)
-			if err != nil {
-				return fmt.Errorf("invalid pattern: %w", err)
-			}
-
-			if err = windows.FindClose(firstFile); err != nil {
-				return fmt.Errorf("failed to close handle: %w", err)
-			}
+		_, err := doublestar.Glob(os.DirFS(basePath), pattern, doublestar.WithFilesOnly())
+		if err != nil {
+			return fmt.Errorf("invalid glob pattern: %w", err)
 		}
 	}
 
@@ -146,15 +118,7 @@ func (c *Collector) Build(_ *slog.Logger, _ *wmi.Client) error {
 func (c *Collector) Collect(_ *types.ScrapeContext, logger *slog.Logger, ch chan<- prometheus.Metric) error {
 	logger = logger.With(slog.String("collector", Name))
 
-	var err error
-
-	if c.config.EnableGlobPattern {
-		err = c.collectGlob(logger, ch)
-	} else {
-		err = c.collectWin32(logger, ch)
-	}
-
-	return err
+	return c.collectGlob(logger, ch)
 }
 
 // collectWin32 collects file times for each file path in the config. It using Win32 FindFirstFile and FindNextFile.
@@ -208,78 +172,6 @@ func (c *Collector) collectGlobFilePath(logger *slog.Logger, ch chan<- prometheu
 			float64(fileInfo.ModTime().UTC().Unix()),
 			filePath,
 		)
-	}
-
-	return nil
-}
-
-// collectWin32 collects file times for each file path in the config. It using Win32 FindFirstFile and FindNextFile.
-func (c *Collector) collectWin32(logger *slog.Logger, ch chan<- prometheus.Metric) error {
-	wg := sync.WaitGroup{}
-
-	for _, filePath := range c.config.filePatterns {
-		wg.Add(1)
-
-		go func(filePath string) {
-			defer wg.Done()
-
-			if err := c.collectWin32FilePath(logger, ch, filePath); err != nil {
-				logger.Error("failed collecting metrics for filepath",
-					slog.String("filepath", filePath),
-					slog.Any("err", err),
-				)
-			}
-		}(filePath)
-	}
-
-	wg.Wait()
-
-	return nil
-}
-
-func (c *Collector) collectWin32FilePath(logger *slog.Logger, ch chan<- prometheus.Metric, filePattern string) error {
-	var data windows.Win32finddata
-	handle, err := windows.FindFirstFile(windows.StringToUTF16Ptr(filePattern), &data)
-	if err != nil {
-		return fmt.Errorf("invalid pattern: %w", err)
-	}
-
-	defer func() {
-		if err := windows.FindClose(handle); err != nil {
-			logger.Warn("failed to close handle",
-				slog.String("filepath", filePattern),
-				slog.Any("err", err),
-			)
-		}
-	}()
-
-	for {
-		fileName := windows.UTF16ToString(data.FileName[:])
-		if fileName != "." && fileName != ".." {
-			fileName = filepath.Join(filepath.Dir(filePattern), fileName)
-
-			ch <- prometheus.MustNewConstMetric(
-				c.fileATime,
-				prometheus.GaugeValue,
-				float64(time.Unix(0, data.LastAccessTime.Nanoseconds()).Unix()),
-				fileName,
-			)
-
-			ch <- prometheus.MustNewConstMetric(
-				c.fileMTime,
-				prometheus.GaugeValue,
-				float64(time.Unix(0, data.LastWriteTime.Nanoseconds()).Unix()),
-				fileName,
-			)
-		}
-
-		if err = windows.FindNextFile(handle, &data); err != nil {
-			if errors.Is(err, windows.ERROR_NO_MORE_FILES) {
-				break
-			}
-
-			return fmt.Errorf("failed to find next file: %w", err)
-		}
 	}
 
 	return nil
