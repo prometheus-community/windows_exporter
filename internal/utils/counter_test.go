0cffc6909b398657fb6501ccd914fb8c5cd38abc
Jan-Otto Kröpke <mail@jkroepke.de>
2024-11-03 18:42:22 +0100

    cpu: implement int32 counter for PDH calls as well.
    Signed-off-by: Jan-Otto Kröpke <mail@jkroepke.de>

---
+++ internal/utils/counter_test.go
@@ -0,0 +1,24 @@
+package utils_test
+
+import (
+	"math"
+	"testing"
+
+	"github.com/prometheus-community/windows_exporter/internal/utils"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCounter(t *testing.T) {
+	t.Parallel()
+
+	c := utils.NewCounter(0)
+	assert.Equal(t, 0.0, c.Value())
+
+	c.AddValue(1)
+
+	assert.Equal(t, 1.0, c.Value())
+
+	c.AddValue(math.MaxUint32)
+
+	assert.Equal(t, float64(math.MaxUint32)+1.0, c.Value())
+}
