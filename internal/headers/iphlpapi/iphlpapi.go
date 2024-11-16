0c3b4e094472e5d2edf01632e79dd52c5c603121
Jan-Otto Kröpke <mail@jkroepke.de>
2024-11-16 20:03:32 +0100

    chore: optimize iphlpapi
    Signed-off-by: Jan-Otto Kröpke <mail@jkroepke.de>

--- internal/headers/iphlpapi/iphlpapi.go
+++ internal/headers/iphlpapi/iphlpapi.go
@@ -16,107 +16,99 @@ var (
 )
 
 func GetTCPConnectionStates(family uint32) (map[MIB_TCP_STATE]uint32, error) {
+	stateCounts := make(map[MIB_TCP_STATE]uint32)
+
+	switch family {
+	case windows.AF_INET:
+		table, err := getExtendedTcpTable[MIB_TCPROW_OWNER_PID](family, TCPTableOwnerPIDAll)
+		if err != nil {
+			return nil, err
+		}
+
+		for _, row := range table {
+			stateCounts[row.dwState]++
+		}
+
+		return stateCounts, nil
+	case windows.AF_INET6:
+		table, err := getExtendedTcpTable[MIB_TCP6ROW_OWNER_PID](family, TCPTableOwnerPIDAll)
+		if err != nil {
+			return nil, err
+		}
+
+		for _, row := range table {
+			stateCounts[row.dwState]++
+		}
+
+		return stateCounts, nil
+	default:
+		return nil, fmt.Errorf("unsupported address family %d", family)
+	}
+}
+
+func GetOwnerPIDOfTCPPort(family uint32, tcpPort uint16) (int, error) {
+	switch family {
+	case windows.AF_INET:
+		table, err := getExtendedTcpTable[MIB_TCPROW_OWNER_PID](family, TCPTableOwnerPIDListener)
+		if err != nil {
+			return 0, err
+		}
+
+		for _, row := range table {
+			if row.dwLocalPort.uint16() == tcpPort {
+				return int(row.dwOwningPid), nil
+			}
+		}
+
+		return 0, fmt.Errorf("no process found for port %d", tcpPort)
+	case windows.AF_INET6:
+		table, err := getExtendedTcpTable[MIB_TCP6ROW_OWNER_PID](family, TCPTableOwnerPIDListener)
+		if err != nil {
+			return 0, err
+		}
+
+		for _, row := range table {
+			if row.dwLocalPort.uint16() == tcpPort {
+				return int(row.dwOwningPid), nil
+			}
+		}
+
+		return 0, fmt.Errorf("no process found for port %d", tcpPort)
+	default:
+		return 0, fmt.Errorf("unsupported address family %d", family)
+	}
+}
+
+func getExtendedTcpTable[T any](ulAf uint32, tableClass uint32) ([]T, error) {
 	var size uint32
 
-	stateCounts := make(map[MIB_TCP_STATE]uint32)
-	rowSize := uint32(unsafe.Sizeof(MIB_TCPROW_OWNER_PID{}))
-	tableClass := TCPTableOwnerPIDAll
+	ret, _, _ := procGetExtendedTcpTable.Call(
+		uintptr(0),
+		uintptr(unsafe.Pointer(&size)),
+		uintptr(0),
+		uintptr(ulAf),
+		uintptr(tableClass),
+		uintptr(0),
+	)
 
-	if family == windows.AF_INET6 {
-		rowSize = uint32(unsafe.Sizeof(MIB_TCP6ROW_OWNER_PID{}))
-		tableClass = TCP6TableOwnerPIDAll
-	}
-
-	ret := getExtendedTcpTable(0, &size, true, family, tableClass, 0)
-	if ret != 0 && ret != uintptr(windows.ERROR_INSUFFICIENT_BUFFER) {
+	if ret != uintptr(windows.ERROR_INSUFFICIENT_BUFFER) {
 		return nil, fmt.Errorf("getExtendedTcpTable (size query) failed with code %d", ret)
 	}
 
 	buf := make([]byte, size)
 
-	ret = getExtendedTcpTable(uintptr(unsafe.Pointer(&buf[0])), &size, true, family, tableClass, 0)
-	if ret != 0 {
-		return nil, fmt.Errorf("getExtendedTcpTable (data query) failed with code %d", ret)
-	}
-
-	numEntries := *(*uint32)(unsafe.Pointer(&buf[0]))
-
-	for i := range numEntries {
-		var state MIB_TCP_STATE
-
-		if family == windows.AF_INET6 {
-			row := (*MIB_TCP6ROW_OWNER_PID)(unsafe.Pointer(&buf[4+i*rowSize]))
-			state = row.dwState
-		} else {
-			row := (*MIB_TCPROW_OWNER_PID)(unsafe.Pointer(&buf[4+i*rowSize]))
-			state = row.dwState
-		}
-
-		stateCounts[state]++
-	}
-
-	return stateCounts, nil
-}
-
-func GetOwnerPIDOfTCPPort(family uint32, tcpPort uint32) (uint32, error) {
-	var size uint32
-
-	stateCounts := make(map[MIB_TCP_STATE]uint32)
-	rowSize := uint32(unsafe.Sizeof(MIB_TCPROW_OWNER_PID{}))
-	tableClass := TCPTableOwnerPIDListener
-
-	if family == windows.AF_INET6 {
-		rowSize = uint32(unsafe.Sizeof(MIB_TCP6ROW_OWNER_PID{}))
-	}
-
-	ret := getExtendedTcpTable(0, &size, true, family, tableClass, 0)
-	if ret != 0 && ret != uintptr(windows.ERROR_INSUFFICIENT_BUFFER) {
-		return 0, fmt.Errorf("getExtendedTcpTable (size query) failed with code %d", ret)
-	}
-
-	buf := make([]byte, size)
-
-	ret = getExtendedTcpTable(uintptr(unsafe.Pointer(&buf[0])), &size, true, family, tableClass, 0)
-	if ret != 0 {
-		return nil, fmt.Errorf("getExtendedTcpTable (data query) failed with code %d", ret)
-	}
-
-	numEntries := binary.LittleEndian.Uint32(buf)
-	entries := (*[1 << 24]MIB_TCPROW_OWNER_PID)(unsafe.Pointer(&rawbuf[4]))[:count:count]
-	for i := range numEntries {
-		var state MIB_TCP_STATE
-
-		if family == windows.AF_INET6 {
-			row := (*MIB_TCP6ROW_OWNER_PID)(unsafe.Pointer(&buf[4+i*rowSize]))
-			state = row.dwState
-		} else {
-			row := (*MIB_TCPROW_OWNER_PID)(unsafe.Pointer(&buf[4+i*rowSize]))
-			state = row.dwState
-		}
-
-		stateCounts[state]++
-	}
-
-	return stateCounts, nil
-}
-
-func getExtendedTcpTable(pTCPTable uintptr, pdwSize *uint32, bOrder bool, ulAf uint32, tableClass uint32, reserved uint32) uintptr {
-	ret, _, _ := procGetExtendedTcpTable.Call(
-		pTCPTable,
-		uintptr(unsafe.Pointer(pdwSize)),
-		uintptr(boolToInt(bOrder)),
+	ret, _, _ = procGetExtendedTcpTable.Call(
+		uintptr(unsafe.Pointer(&buf[0])),
+		uintptr(unsafe.Pointer(&size)),
+		uintptr(0),
 		uintptr(ulAf),
 		uintptr(tableClass),
-		uintptr(reserved),
+		uintptr(0),
 	)
 
-	return ret
-}
-
-func boolToInt(b bool) int {
-	if b {
-		return 1
+	if ret != 0 {
+		return nil, fmt.Errorf("getExtendedTcpTable (data query) failed with code %d", ret)
 	}
 
-	return 0
+	return unsafe.Slice((*T)(unsafe.Pointer(&buf[4])), binary.LittleEndian.Uint32(buf)), nil
 }
