4c2bac8340071d294f5bf0e20b11ed93ff98d157
Jan-Otto Kröpke <mail@jkroepke.de>
2024-11-24 14:42:26 +0100

    chore: move exporter binary to cmd package
    Signed-off-by: Jan-Otto Kröpke <mail@jkroepke.de>

--- internal/windowsservice/windowsservice.go
+++ internal/windowsservice/windowsservice.go
@@ -11,18 +11,15 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// windowsservice allows us to initiate Time Sensitive components (Like registering the windows service) as early as possible in the startup process
 //go:build windows
 
 package windowsservice
 
 import (
 	"fmt"
-	"os"
 
 	"golang.org/x/sys/windows"
 	"golang.org/x/sys/windows/svc"
-	"golang.org/x/sys/windows/svc/eventlog"
 )
 
 const (
@@ -31,83 +28,37 @@ const (
 
 type windowsExporterService struct{}
 
+// Execute is the entry point for the Windows service manager.
 func (s *windowsExporterService) Execute(_ []string, r <-chan svc.ChangeRequest, changes chan<- svc.Status) (bool, uint32) {
-	const cmdsAccepted = svc.AcceptStop | svc.AcceptShutdown
 	changes <- svc.Status{State: svc.StartPending}
-	changes <- svc.Status{State: svc.Running, Accepts: cmdsAccepted}
+	changes <- svc.Status{State: svc.Running, Accepts: svc.AcceptStop | svc.AcceptShutdown}
 
 	for {
 		select {
 		case exitCodeCh := <-ExitCodeCh:
+			// Stop the service if an exit code from the main function is received.
 			changes <- svc.Status{State: svc.StopPending}
 
 			return true, uint32(exitCodeCh)
 		case c := <-r:
+			// Handle the service control request.
 			switch c.Cmd {
 			case svc.Interrogate:
 				changes <- c.CurrentStatus
 			case svc.Stop, svc.Shutdown:
+				// Stop the service if a stop or shutdown request is received.
 				_ = logToEventToLog(windows.EVENTLOG_INFORMATION_TYPE, "service stop received")
 
 				changes <- svc.Status{State: svc.StopPending}
 
-				return false, 0
+				// Send a signal to the main function to stop the service.
+				StopCh <- struct{}{}
+
+				// Wait for the main function to stop the service.
+				return false, uint32(<-ExitCodeCh)
 			default:
 				_ = logToEventToLog(windows.EVENTLOG_ERROR_TYPE, fmt.Sprintf("unexpected control request #%d", c))
 			}
 		}
 	}
-}
-
-var (
-	IsService  bool
-	ExitCodeCh = make(chan int)
-	StopCh     = make(chan struct{})
-)
-
-//nolint:gochecknoinits
-func init() {
-	var err error
-
-	IsService, err = svc.IsWindowsService()
-	if err != nil {
-		err = logToEventToLog(windows.EVENTLOG_ERROR_TYPE, fmt.Sprintf("Failed to detect service: %v", err))
-		if err != nil {
-			os.Exit(2)
-		}
-
-		os.Exit(1)
-	}
-
-	if IsService {
-		err = logToEventToLog(windows.EVENTLOG_INFORMATION_TYPE, "Attempting to start exporter service")
-
-		go func() {
-			err = svc.Run(serviceName, &windowsExporterService{})
-			if err != nil {
-				_ = logToEventToLog(windows.EVENTLOG_ERROR_TYPE, fmt.Sprintf("Failed to start service: %v", err))
-			}
-
-			StopCh <- struct{}{}
-		}()
-	}
-}
-
-func logToEventToLog(eType uint16, msg string) error {
-	eventLog, err := eventlog.Open("windows_exporter")
-	if err != nil {
-		return fmt.Errorf("failed to open event log: %w", err)
-	}
-	defer func(eventLog *eventlog.Log) {
-		_ = eventLog.Close()
-	}(eventLog)
-
-	p, err := windows.UTF16PtrFromString(msg)
-	if err != nil {
-		return fmt.Errorf("error convert string to UTF-16: %w", err)
-	}
-
-	ss := []*uint16{p, nil, nil, nil, nil, nil, nil, nil, nil}
-
-	return windows.ReportEvent(eventLog.Handle, eType, 0, 3299, 0, 9, 0, &ss[0], nil)
 }
