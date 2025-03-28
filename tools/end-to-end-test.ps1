95b7ee526c709ce4f99cebf5cc5434679f8411f0
Jan-Otto Kröpke <mail@jkroepke.de>
2025-03-28 12:53:57 +0100

    nic: expose operation status of nic
    Signed-off-by: Jan-Otto Kröpke <mail@jkroepke.de>

--- tools/end-to-end-test.ps1
+++ tools/end-to-end-test.ps1
@@ -70,7 +70,7 @@ $output_diff = Compare-Object ((Get-Content 'e2e-output.txt' | Out-String).Trim(
 
 # Fail if differences in output are detected
 if (-not ($null -eq $output_diff)) {
-    $output_diff | Format-Table -AutoSize | Out-String -Width 10000
+    $output_diff | Format-Table -AutoSize | Out-String -Width 1000
 
     Write-Host "STDOUT"
     Get-Content "$($temp_dir)/windows_exporter.log"
