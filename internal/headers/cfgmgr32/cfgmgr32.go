0b8a257b31d35b918deb224b56b701b4242dcaf6
Jan-Otto Kröpke <mail@jkroepke.de>
2025-08-28 06:36:10 +0200

    gpu: add device id label (#2186)

---
+++ internal/headers/cfgmgr32/cfgmgr32.go
@@ -0,0 +1,92 @@
+// SPDX-License-Identifier: Apache-2.0
+//
+// Copyright The Prometheus Authors
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cfgmgr32
+
+import (
+	"fmt"
+	"unsafe"
+
+	"github.com/prometheus-community/windows_exporter/internal/headers/win32"
+	"golang.org/x/sys/windows"
+)
+
+func GetDevicesInstanceIDs(deviceID string) ([]Device, error) {
+	var (
+		err      error
+		listSize uint32
+	)
+
+	deviceIDLWStr := win32.NewLPWSTR(deviceID)
+
+	err = CMGetDeviceIDListSize(deviceIDLWStr, &listSize)
+	if err != nil {
+		return nil, err
+	}
+
+	listBuffer := make([]uint16, listSize)
+
+	err = CMGetDeviceIDList(deviceIDLWStr, listBuffer)
+	if err != nil {
+		return nil, err
+	}
+
+	deviceInstanceIDs := win32.ParseMultiSz(listBuffer)
+	devices := make([]Device, 0, len(deviceInstanceIDs))
+
+	for _, deviceInstanceID := range deviceInstanceIDs {
+		var devNode *windows.Handle
+
+		err = CMLocateDevNode(&devNode, deviceInstanceID)
+		if err != nil {
+			return nil, err
+		}
+
+		var (
+			busNumber     uint32
+			deviceAddress uint32
+			propType      uint32
+		)
+
+		propLen := uint32(4)
+
+		err = CMGetDevNodeProperty(devNode, DEVPKEYDeviceBusNumber, &propType, unsafe.Pointer(&busNumber), &propLen)
+		if err != nil {
+			return nil, err
+		}
+
+		if propType != DEVPROP_TYPE_UINT32 {
+			return nil, fmt.Errorf("unexpected property type: 0x%08X", propType)
+		}
+
+		err = CMGetDevNodeProperty(devNode, DEVPKEYDeviceAddress, &propType, unsafe.Pointer(&deviceAddress), &propLen)
+		if err != nil {
+			return nil, err
+		}
+
+		if propType != DEVPROP_TYPE_UINT32 {
+			return nil, fmt.Errorf("unexpected property type: 0x%08X", propType)
+		}
+
+		devices = append(devices, Device{
+			InstanceID:     windows.UTF16ToString(deviceInstanceID),
+			BusNumber:      win32.UINT(busNumber),
+			DeviceNumber:   win32.UINT(deviceAddress >> 16),
+			FunctionNumber: win32.UINT(deviceAddress & 0xFFFF),
+		})
+	}
+
+	return devices, nil
+}
---
+++ internal/headers/cfgmgr32/types.go
@@ -0,0 +1,70 @@
+// SPDX-License-Identifier: Apache-2.0
+//
+// Copyright The Prometheus Authors
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cfgmgr32
+
+import (
+	"github.com/go-ole/go-ole"
+	"github.com/prometheus-community/windows_exporter/internal/headers/win32"
+)
+
+const (
+	// Configuration Manager return codes
+	CR_SUCCESS = 0x00
+
+	// Filter flags
+	CM_GETIDLIST_FILTER_ENUMERATOR = 0x00000001
+	CM_GETIDLIST_FILTER_PRESENT    = 0x00000100
+
+	DEVPROP_TYPE_UINT32 uint32 = 0x00000007
+)
+
+// DEVPROPKEY represents a device property key (GUID + pid)
+type DEVPROPKEY struct {
+	FmtID ole.GUID
+	PID   uint32
+}
+
+type Device struct {
+	InstanceID     string
+	BusNumber      win32.UINT
+	DeviceNumber   win32.UINT
+	FunctionNumber win32.UINT
+}
+
+//nolint:gochecknoglobals
+var (
+	// https://github.com/Infinidat/infi.devicemanager/blob/8be9ead6b04ff45c63d9e3bc70d82cceafb75c47/src/infi/devicemanager/setupapi/properties.py#L138C1-L143C34
+	DEVPKEYDeviceBusNumber = &DEVPROPKEY{
+		FmtID: ole.GUID{
+			Data1: 0xa45c254e,
+			Data2: 0xdf1c,
+			Data3: 0x4efd,
+			Data4: [8]byte{0x80, 0x20, 0x67, 0xd1, 0x46, 0xa8, 0x50, 0xe0},
+		},
+		PID: 23, // DEVPROP_TYPE_UINT32
+	}
+
+	// https://github.com/Infinidat/infi.devicemanager/blob/8be9ead6b04ff45c63d9e3bc70d82cceafb75c47/src/infi/devicemanager/setupapi/properties.py#L187-L192
+	DEVPKEYDeviceAddress = &DEVPROPKEY{
+		FmtID: ole.GUID{
+			Data1: 0xa45c254e,
+			Data2: 0xdf1c,
+			Data3: 0x4efd,
+			Data4: [8]byte{0x80, 0x20, 0x67, 0xd1, 0x46, 0xa8, 0x50, 0xe0},
+		},
+		PID: 30, // DEVPROP_TYPE_UINT32
+	}
+)
